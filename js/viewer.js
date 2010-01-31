4877ac59654011d4aca29ed057346e978af34801
ido <ido@ido-yehielis-mac-mini.local>
2010-01-31 17:48:00 +0100

    drawing pics for fighter

--- js/viewer.js
+++ js/viewer.js
@@ -2,6 +2,10 @@
 var Viewer = function(width, height){
 	var canvas;
 	var center = [width / 2, height / 2];
+	var playerImg = new Image();
+	playerImg.src = 'pics/chars.png';
+	var tilesImg = new Image();
+	tilesImg.src = '../pics/tiles-big.png';
 	
 	var Canvas = function(){
 		var canvasElement;
@@ -32,13 +36,15 @@ var Viewer = function(width, height){
 		
 		canvasElement = $("#viewer").get()[0];
 		context = canvasElement.getContext("2d");
-		changeFont("14px monospace");
+		changeFont("32px monospace");
+		tileSize= 32;
 		
 		return {
 			canvasElement: canvasElement,
 			context: context,
 			fontHeight: fontHeight,
-			fontWidth: fontWidth
+			fontWidth: fontWidth,
+			tileSize: tileSize,
 		}
 	}
 	var clear = function(){
@@ -46,8 +52,12 @@ var Viewer = function(width, height){
 		canvas.context.fillRect(0, 0, canvas.canvasElement.width, canvas.canvasElement.height);
 	}
 	var putTile = function(x, y, symbol, color){
-		canvas.context.fillStyle = "rgb(" + color[0] + "," + color[1] + "," + color[2] + ")";
-		canvas.context.fillText(symbol, x * canvas.fontWidth, y * canvas.fontHeight);
+		if(symbol=='@')
+			canvas.context.drawImage(playerImg, 0, 0, canvas.tileSize, canvas.tileSize, x * canvas.tileSize, y * canvas.tileSize, canvas.tileSize ,canvas.tileSize);
+		else {
+			canvas.context.fillStyle = "rgb(" + color[0] + "," + color[1] + "," + color[2] + ")";
+			canvas.context.fillText(symbol, x * canvas.tileSize, y * canvas.tileSize);
+		}
 	}
 	var print = function(x, y, str, color){
 		canvas.context.fillStyle = "rgb(" + color[0] + "," + color[1] + "," + color[2] + ")";
