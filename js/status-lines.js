5b3073c243e548c2e408c4f2f85f2cba661964a8
Corey Martin <nefariousd@gmail.com>
2010-11-05 15:28:26 -0400

    Stats are now being properly shown and updated in the new stats panel. Vitals will be implemented next.

--- js/status-lines.js
+++ js/status-lines.js
@@ -99,8 +99,58 @@ var StatusLines = function(){
 				.toggleClass('chargeBarFlash', 25);
 		}
 		if ( player && player.vars.spiritPoints < 360 ) chargeFlashed = false;
+
+		printNew(empty);
 	}
-	
+
+	var renderStat = function(name, value, buffed) {
+		var cssClass = '';
+		if ( buffed < 0 ) {
+			cssClass = 'statDebuffed';
+		} else if ( buffed > 0 ) {
+			cssClass = 'statBuffed';
+		}
+		var str = name+": <label class='"+cssClass+"'>"+value+"</label><br />";
+		//element.html(str);
+		return str;
+	}
+
+	var printNew = function(empty) {
+		if (empty ) {
+			return;
+		}
+		// Abilities
+		var vit=player.vars.vitality;
+		var atk=player.vars.attack+getBuff("attack");
+		var def=player.vars.defense+getBuff("defense");
+		var spd=player.vars.speed+getBuff("speed");
+		var spr=player.vars.spirit+getBuff("spirit");
+		// stats
+		var dmgMultipler = 1;
+		if(player.vars.buffs && player.vars.buffs.damageMultipler && player.vars.buffs.damageMultipler!=0)
+			dmgMultipler = player.vars.buffs.damageMultipler * 1;
+		
+		// natural damage
+		var dmgRange=player.vars.damage;
+		// weapon damage
+		if(player.vars.weapon.wielded[0])
+			dmgRange= player.vars.weapon.wielded[0].vars.damage;
+
+		var lif = player.vars.life+getBuff("life");
+		var mlif = player.vars.maxLife+getBuff("life");	
+
+		var statsPanel = $("#statsPanel");
+		statsPanel.empty();
+		var output = '';
+		output += renderStat('Damage', dmgRange[0]==dmgRange[1]?dmgRange[0]*dmgMultipler:(dmgRange[0]*dmgMultipler+"-"+dmgRange[1]*dmgMultipler), 0);
+		output += renderStat('Attack', atk, getBuff("attack"));
+		output += renderStat('Defense', def, getBuff("defense"));
+		output += renderStat('Vitality', vit, getBuff("vitality"));
+		output += renderStat('Speed', spd, getBuff("speed"));
+		output += renderStat('Spirit', spr, getBuff("spirit"));
+		statsPanel.html(output);
+	}
+
 	return {
 		print: print
 	}
